e03de0cb0cff082f56c3e8b5a9b33cbbe7be0338
Ammar Sijak# <ammar.sijak@outlook.com>
2018-11-20 11:45:23 +0100

    first version

---
+++ car.cpp
@@ -0,0 +1,45 @@
+#include "car.h"
+#include <stdlib.h>
+
+Car get_car(CarType car1)
+{
+  return 0;
+}
+
+CarType get_type(Car car1)
+{
+  return JEEP;
+}
+
+Color get_color(Car car1)
+{
+  return SILVER;
+}
+
+double get_fill_level(Car car1)
+{
+  return 80.0;
+}
+
+double get_acceleration_rate(Car car1)
+{
+  return 0.0;
+}
+
+double set_acceleration_rate(Car car1, double acc)
+{
+  return 0.0;
+}
+int get_speed(Car car1)
+{
+  return 0;
+}
+
+void accelerate(Car car1)
+{
+
+}
+void init()
+{
+
+}
--- car.h
+++ car.h
@@ -3,8 +3,8 @@
 * ---------------------------------------------------------
 * Exercise Number: 0
 * Title:			car.h
-* Author:			P. Bauer
-* Due Date:		January 9, 2015
+* Author:			Ammar Sijak
+* Due Date:		November 20, 2018
 * ----------------------------------------------------------
 * Description:
 * Car abstract data type demo.
@@ -13,4 +13,18 @@
 #ifndef ___CAR_H
 #define ___CAR_H
 
+enum  CarType{AIXAM, FIAT_MULTIPLA, JEEP, };
+enum Color{RED, GREEN, BLUE, ORANGE, SILVER, BLACK};
+
+typedef struct CarImplementation* Car;
+
+Car get_car(CarType car1);
+CarType get_type(Car car1);
+Color get_color(Car car1);
+double get_fill_level(Car car1);
+double get_acceleration_rate(Car car1);
+double set_acceleration_rate(Car car1, double acc );
+int get_speed(Car car1);
+void accelerate(Car car1);
+void init();
 #endif
